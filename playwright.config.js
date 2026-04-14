834dbc62ed2a9e44f83b7749123310c20d90f44f
Marbiya07 <marbiya.muhtar7@gmail.com>
2026-04-14 11:35:56 +0300

    Set headless mode to false in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     screenshot: { mode: "on" },
     video: "retain-on-failure",
     //video: {mode: 'retain-on-failure', size: {width: 1920, height: 1080} },
-    //executablePath: "/Applications/Firefox.app/Contents/MacOS/firefox",
+    headless: false,
   },
 
   /* Configure projects for major browsers */
